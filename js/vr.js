e4cf570993e35d443a4c852b89e7e25b91a6aeb7
Kevin <kevin-vr@users.noreply.github.com>
2018-01-08 13:40:31 -0500

    update files

--- js/vr.js
+++ js/vr.js
@@ -27,27 +27,18 @@ function ptc(string) {
   console.log("vr webview: " + string);
 }
 
-function ptc2(string, number) {
-  console.log("vr webview: " + string + " and " + number);
-}
-
-/*
-function start(URIJSON) {
-  console.log("vr: start called");
-  console.log(URIJSON);
-  sceneJSONString = decodeURIComponent(URIJSON);
-  console.log(sceneJSONString);
-  initVR();
-}
-*/
-
 function initVR() {
+  var polyfill = new WebVRPolyfill();
   var vrButton = document.getElementById("vr-switch");
   var playButton = document.getElementById("vr-play");
   var playSim = false;
   vrButton.addEventListener("click", function() {
     windowHeight = window.innerHeight;
-    vrDisplay.requestPresent([{source: renderer.domElement}]);
+    if (vrDisplay) {
+      vrDisplay.requestPresent([{source: renderer.domElement}]);
+    } else {
+      window.alert("VR not supported: navigator.getVRDisplays() is empty.");
+    }
   });
   playButton.addEventListener("click", function() {
     if (playSim) {
@@ -67,7 +58,7 @@ function initVR() {
 
   var shadows;
 
-  var scene, vrControls, vrEffect, vrDisplay;;
+  var scene, controls, vrEffect, vrDisplay;;
   var dummy, camera;
 
   var cameraCube, sceneCube, equirectMaterial;
@@ -168,10 +159,14 @@ function initVR() {
         }
     });
 
-    navigator.getVRDisplays().then(function(displays) {
-        if (displays.length > 0) {
-          vrDisplay = displays[0];
+    navigator.getVRDisplays().then(function(vrDisplays) {
+        if (vrDisplays.length) {
+          vrDisplay = vrDisplays[0];
+          controls = new THREE.VRControls(dummy);
           vrDisplay.requestAnimationFrame(render);
+        } else {
+          controls = new THREE.OrbitControls(dummy, renderer.domElement);
+          requestAnimationFrame(render);
         }
       });
 
@@ -267,7 +262,6 @@ function initVR() {
     camera = new THREE.PerspectiveCamera(60, window.innerWidth / window.innerHeight, 0.01, 10000);
     camera.position.set(0, 0, 0);
     dummy.add(camera);
-    vrControls = new THREE.VRControls(camera);
     if (worldJSON.background !== "") {
       var equirectTexture = new THREE.TextureLoader().load(worldJSON.background);
       equirectTexture.mapping = THREE.EquirectangularReflectionMapping;
@@ -387,8 +381,6 @@ function initVR() {
   }
 
   function onWindowResize() {
-    console.log("window resize");
-    console.log("window resize not presenting");
     camera.aspect = window.innerWidth / window.innerHeight;
     camera.updateProjectionMatrix();
 
@@ -405,7 +397,11 @@ function initVR() {
   }
 
   function render() {
-    vrDisplay.requestAnimationFrame(render);
+    if (vrDisplay) {
+      vrDisplay.requestAnimationFrame(render);
+    } else {
+      requestAnimationFrame(render);
+    }
     if (playSim) {
       var deltaTime = clock.getDelta();
       updatePhysics(deltaTime);
@@ -413,7 +409,7 @@ function initVR() {
         frameUpdate();
       }
     }
-    vrControls.update();
+    controls.update();
     Reticulum.update();
     renderer.clear();
     cameraCube.rotation.copy(camera.rotation);
